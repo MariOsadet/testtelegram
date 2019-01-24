47cfd8e741f95b210b074de65412cd744e7e94d6
mariiaosadets <mariia.osadets@vitech.com.ua>
2019-01-24 14:44:22 +0200

    extend test

--- src/test/java/redis/RedisObjectTest.java
+++ src/test/java/redis/RedisObjectTest.java
@@ -10,6 +10,7 @@ public class RedisObjectTest {
         final RedisObject redisObject = new RedisObject("Test", "0");
         Assert.assertEquals("0", redisObject.getIdAsString());
         Assert.assertEquals("Test", redisObject.getFileId());
+        Assert.assertFalse(redisObject.isProcessed());
     }
 
 }
